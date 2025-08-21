b6685c398bbfb71a5a051ca32c472b9e99d95b4b
avanky1 <onlyfreematerials@gmail.com>
2025-08-22 01:48:19 +0530

    models updated

--- lib/db.ts
+++ lib/db.ts
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if(!MONGODB_URI){
-     throw new Error("please define mongodb url in .env file");
+    throw new Error("please define mongodb url in .env file");
 }
 
 let cached = global.mongoose;
 
 if(!cached){
-     cached = global.mongoose = { conn: null, promise: null };
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
