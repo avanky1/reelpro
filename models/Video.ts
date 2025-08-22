b3e588ef675e02c9323da1ead43dfe5974f35119
avanky1 <onlyfreematerials@gmail.com>
2025-08-22 11:53:03 +0530

    video.ts

--- models/Video.ts
+++ models/Video.ts
@@ -1,1 +1,68 @@
-import 
+import { timeStamp } from "console";
+import mongoose, { Schema, model, models, mongo } from "mongoose";
+
+
+export const VIDEO_DIMENSIONS = {
+  width: 1080,
+  height: 1920,
+} as const;
+
+
+
+export interface IVideo{
+  _id?: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  controls?: boolean;
+  transformation?: {
+    height: number;
+    width: number;
+    quality?: number;
+  }
+
+}
+
+
+const VideoSchema = new Schema<IVideo>({
+  title:{
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  videoUrl: {
+    type: String,
+    required: true
+  },
+  thumbnailUrl: {
+    type: String,
+    required: true
+  },
+  controls: {
+    type: Boolean,
+    default: true
+  },
+  transformation: {
+    height: {
+      type: Number,
+      default: VIDEO_DIMENSIONS.height,
+      required: true
+    },
+    width: {
+      type: Number,
+      default: VIDEO_DIMENSIONS.width,
+      required: true
+    },
+    quality: {
+      type: Number,
+      min:1,
+      max:100,
+    }
+  }
+}, { timestamps: true})
+
+const Video = models.Video || model<IVideo>("Video", VideoSchema);
