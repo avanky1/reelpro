2224719a2a054f0a7388769e7e772b094396f381
avanky1 <onlyfreematerials@gmail.com>
2025-08-22 14:48:46 +0530

    lib/auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,73 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions : NextAuthOptions = {
+
+     providers:[
+          CredentialsProvider(
+               {
+                    name: "Credentials",
+                    credentials:{
+                         email: { label: "Email", type: "text" },
+                         password: { label: "Password", type: "password" }
+                    },
+                    async authorize(credentials) {
+                        if(!credentials?.email || !credentials.password) {
+                            throw new Error("Please enter your email and password");
+                        }
+
+                        try {
+                            await connectToDatabase();
+
+                            const user = await User.findOne({ email: credentials.email });
+                           
+                            if(!user){
+                                throw new Error("No user found");
+                            }
+
+                            const isValid = await bcrypt.compare(credentials.password, user.password);
+                            if (!isValid) {
+                                throw new Error("Invalid email or password");
+                            }
+
+                            return {
+                              id: user._id.toString(),
+                              email: user.email
+                            }
+
+                        } catch (error) {
+                            throw new Error("Internal error");
+                        }
+                    }
+
+               }
+          )
+               
+     ],
+     callbacks:{
+          async jwt({token,  user}){
+            if(user){
+                token.id = user.id;
+            }
+            return token;
+          },
+
+          async session({session, token}){
+
+            if(session.user){
+                session.user.id = token.id as string
+            }
+
+            return session
+
+          }
+     },
+
+     session: {
+         strategy: "jwt"
+     }
+
+}
