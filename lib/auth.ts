4cae37fc36209bfc37cdb604a7b6c33e1dbc4f1a
avanky1 <onlyfreematerials@gmail.com>
2025-08-22 15:13:18 +0530

    middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -65,9 +65,17 @@ export const authOptions : NextAuthOptions = {
 
           }
      },
+     pages:{
+        signIn: "/login",
+        error: "/login"
+     },
 
      session: {
-         strategy: "jwt"
-     }
+         strategy: "jwt",
+         maxAge: 30 * 24 * 60 * 60
+     },
+     secret: process.env.NEXTAUTH_SECRET
 
-}
+};
+
+
---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import path from "path";
+
+export default withAuth(
+     function middleware(){
+          return NextResponse.next();
+     },
+     {
+          callbacks: {
+               authorized: ({token, req}) => {
+                    const {pathname} = req.nextUrl;
+                    if (pathname.startsWith("/api/auth") || (pathname.startsWith("/login") || (pathname.startsWith("/register")))) {
+                         return true;
+                    }
+
+                    if(pathname === "/" || pathname.startsWith("/api/videos")) {
+                         return !!token;
+                    }
+                    return !!token;
+               }
+          }
+     }
+)
+
+ export const config = {
+          matcher: [
+               "/((?!_next/static|_next/image|favicon.ico|public/).*))"
+          ]
+     }
